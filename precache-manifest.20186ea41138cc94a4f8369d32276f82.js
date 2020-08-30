aeee8845c5c8f7f5c95ab01c1f45bd5b7f9bb352
Spikeedoo <spikeedoo14@gmail.com>
2020-08-30 11:52:13 -0500

    Updates

---
+++ precache-manifest.20186ea41138cc94a4f8369d32276f82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8170fb508055c465132801bee33c2e95",
+    "url": "/minor-prophets/index.html"
+  },
+  {
+    "revision": "304cbfecf594252055bb",
+    "url": "/minor-prophets/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d80c652b9fc6d97effb",
+    "url": "/minor-prophets/static/js/2.64aa8c65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minor-prophets/static/js/2.64aa8c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304cbfecf594252055bb",
+    "url": "/minor-prophets/static/js/main.25b40d0a.chunk.js"
+  },
+  {
+    "revision": "c1f002aab613dba85d4e",
+    "url": "/minor-prophets/static/js/runtime-main.a382f392.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/minor-prophets/static/media/logo.5d5d9eef.svg"
+  }
+]);
